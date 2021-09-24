d8a13d6a4f96bd2bac8594b62f982adcce155dfd
sathinka <sathinkawijesingha123@gmail.com>
2021-09-25 00:29:37 +0530

    create add review

---
+++ app/src/main/java/com/example/mad_project_2021/TotalQuantity.java
@@ -0,0 +1,40 @@
+package com.example.mad_project_2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class TotalQuantity extends AppCompatActivity {
+    EditText etnum1,etnum2,etnum3;
+    TextView textV, tvResult;
+    Button btnAdd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_total_quantity);
+        etnum1=(EditText)findViewById(R.id.etnum1);
+        etnum2=(EditText)findViewById(R.id.etnum2);
+        etnum3=(EditText)findViewById(R.id.etnum3);
+        textV=(TextView)findViewById(R.id.tvResult);
+        btnAdd=(Button)findViewById(R.id.btnAdd);
+        onAdd();
+    }
+    private void onAdd(){
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1=Integer.parseInt(etnum1.getText().toString());
+                int num2=Integer.parseInt(etnum2.getText().toString());
+                int num3=Integer.parseInt(etnum3.getText().toString());
+                int result=num1+num2+num3;
+                textV.setText(Integer.toString(result));
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/mad_project_2021/addtocart.java
+++ app/src/main/java/com/example/mad_project_2021/addtocart.java
@@ -2,6 +2,7 @@ package com.example.mad_project_2021;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Button;
 import android.view.View;
@@ -11,8 +12,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
@@ -22,6 +26,8 @@ public class addtocart extends AppCompatActivity {
     EditText textquantity3;
     Button buttoncheckout;
     Button btnUpdate;
+    Button btnRet;
+    Button btnDelete;
     DatabaseReference reff;
     Quantity quantity;
 
@@ -35,73 +41,120 @@ public class addtocart extends AppCompatActivity {
         textquantity2 = (EditText) findViewById((R.id.textquantity2));
         textquantity3 = (EditText) findViewById((R.id.textquantity3));
         buttoncheckout = (Button) findViewById(R.id.buttoncheckout);
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        btnRet = (Button) findViewById(R.id.btnRet);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
         quantity = new Quantity();
         reff = FirebaseDatabase.getInstance().getReference().child("Quantity");
         buttoncheckout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                try{
+                try {
                     int qty1 = Integer.parseInt(textquantity1.getText().toString().trim());
-                    quantity.setQty1(qty1);}
-                catch (NumberFormatException nfe) {
+                    quantity.setQty1(qty1);
+                } catch (NumberFormatException nfe) {
                     nfe.printStackTrace();
                 }
 
 
-                try{
+                try {
                     int qty2 = Integer.parseInt(textquantity2.getText().toString().trim());
-                    quantity.setQty2(qty2);}
-                catch (NumberFormatException nfe) {
+                    quantity.setQty2(qty2);
+                } catch (NumberFormatException nfe) {
                     nfe.printStackTrace();
                 }
 
-                try{
+                try {
                     int qty3 = Integer.parseInt(textquantity3.getText().toString().trim());
-                    quantity.setQty3(qty3);}
-                catch (NumberFormatException nfe) {
+                    quantity.setQty3(qty3);
+                } catch (NumberFormatException nfe) {
                     nfe.printStackTrace();
                 }
-
-
                 reff.push().setValue(quantity);
-                Toast.makeText(addtocart.this,"data inserted successfully", Toast.LENGTH_LONG).show();
-
-
+                Toast.makeText(addtocart.this, "quantity inserted successfully", Toast.LENGTH_LONG).show();
 
 
             }
         });
+        //Retrieve
+        btnRet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reff = FirebaseDatabase.getInstance().getReference().child("Quantity").child("quantity1");
+                reff.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String qty1 = dataSnapshot.child("qty1").getValue().toString();
+                        String qty2 = dataSnapshot.child("qty2").getValue().toString();
+                        String qty3 = dataSnapshot.child("qty3").getValue().toString();
+
+                        textquantity1.setText(qty1);
+                        textquantity2.setText(qty2);
+                        textquantity3.setText(qty3);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
 
         //update
-        btnUpdate.setOnClickListener(new View.OnClickListener(){
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View v){
-                      int qty1=Integer.parseInt(textquantity1.getText().toString().trim());
-                int qty2=Integer.parseInt(textquantity2.getText().toString().trim());
-                int qty3=Integer.parseInt(textquantity3.getText().toString().trim());
-                updateQunatity(qty1,qty2,qty3);
+            public void onClick(View v) {
+                int q1 = Integer.parseInt(textquantity1.getText().toString().trim());
+                int q2 = Integer.parseInt(textquantity2.getText().toString().trim());
+                int q3 = Integer.parseInt(textquantity3.getText().toString().trim());
+                updateQuantity(q1, q2, q3);
             }
         });
+
     }
 
-    private void updateQunatity(int qty1, int qty2, int qty3){
+    private void updateQuantity(int qty1, int qty2, int qty3) {
         HashMap quantity = new HashMap();
         quantity.put("qty1", qty1);
         quantity.put("qty2", qty2);
         quantity.put("qty3", qty3);
 
 
-        DatabaseReference upRef = FirebaseDatabase.getInstance().getReference().child("Quantity").child("quantity2");
+        DatabaseReference upRef = FirebaseDatabase.getInstance().getReference().child("Quantity").child("quantity1");
         upRef.updateChildren(quantity).addOnCompleteListener(new OnCompleteListener() {
             @Override
             public void onComplete(@NonNull Task task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(addtocart.this, "updated successfully!", Toast.LENGTH_SHORT).show();
+                if (task.isSuccessful()) {
+                    Toast.makeText(addtocart.this, " quantity updated successfully!", Toast.LENGTH_SHORT).show();
 
-                }else{
+                } else {
                     Toast.makeText(addtocart.this, "Updating Unsuccessful", Toast.LENGTH_SHORT).show();
                 }
+            }
+
+
+        });
+
+        //delete
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                DatabaseReference delRef = FirebaseDatabase.getInstance().getReference().child("Quantity").child("quantity1");
+                delRef.removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(addtocart.this, "quantity deleted", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText(addtocart.this, " quantity deleting Failed!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
             }
         });
 
