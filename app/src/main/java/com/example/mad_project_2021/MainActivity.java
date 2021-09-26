6228956aa1eb7b1813ebeed1de195b12a8b76505
Hiran <hiranbuddhika7@gmail.com>
2021-09-26 11:33:36 +0530

    add signout button and logging details

--- app/src/main/java/com/example/mad_project_2021/MainActivity.java
+++ app/src/main/java/com/example/mad_project_2021/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.example.mad_project_2021.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageButton group_60,group_59;
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +38,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intentLoadNewActivity = new Intent(MainActivity.this, AddActivity.class);
                 startActivity(intentLoadNewActivity);
+            }
+        });
+
+        logout = (Button) findViewById(R.id.btnSignOut);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this,activity_login.class));
             }
         });
     }
